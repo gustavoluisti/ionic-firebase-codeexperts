0c7d1d95b5ba32aaa915c08dae5b7391a1ec9f2a
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2018-07-16 06:11:11 -0300

    Auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,49 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseServiceProvider } from '../providers/firebase-service/firebase-service';
+import { SavePage } from '../pages/save/save';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { HousePage } from './../pages/house/house';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SavePage,
+    HousePage,
   ],
   imports: [
-    BrowserModule,
+BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBpsNrg6SY3n7O0I1iwvqWYLzLNfY3eJxQ",
+      authDomain: "fir-projectionic-ac08b.firebaseapp.com",
+      databaseURL: "https://fir-projectionic-ac08b.firebaseio.com",
+      projectId: "fir-projectionic-ac08b",
+      storageBucket: "fir-projectionic-ac08b.appspot.com",
+      messagingSenderId: "637049303690"
+    }),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    SavePage,
+    HousePage
+
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseServiceProvider
   ]
 })
 export class AppModule {}
